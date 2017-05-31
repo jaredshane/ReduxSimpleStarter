526195f446f1c3956bacfe538fac662b66b8728e
Jared Fuller <jaredshane@gmail.com>
2017-05-31 13:23:17 -0500

    added a function to make the YTSearch more flexible

--- src/index.js
+++ src/index.js
@@ -12,10 +12,18 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos })
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
