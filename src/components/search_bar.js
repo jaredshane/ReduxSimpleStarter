55034006d91168b06201414b952d8f164698c68b
Jared Fuller <jaredshane@gmail.com>
2017-05-31 13:18:59 -0500

    refactored the SearchBar to call onInputChange function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,20 +4,24 @@ class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      term: 'Starting Value'
+      term: ''
     }
   }
 
   render() {
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value})}
+          onChange={event => this.onInputChange(event.target.value)}
         />
-          Value of the input: {this.state.term}
       </div>
     )
+  }
+
+  onInputChange(term) {
+    this.setState({ term })
+    this.props.onSearchTermChange(term)
   }
 }
 
