ebe58ad99b1e376bff50c09a3dae08b0d9f58b7f
Jared Fuller <jaredshane@gmail.com>
2017-05-31 13:24:21 -0500

    added VideoDetail component to the render and passed some props to other components

--- src/index.js
+++ src/index.js
@@ -31,8 +31,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
       </div>
     )
   }
