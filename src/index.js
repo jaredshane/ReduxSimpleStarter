332dd5df1aca65304c14c9298b400292092e2a9b
Jared Fuller <jaredshane@gmail.com>
2017-05-31 11:41:13 -0500

    refactoed the App component to be a stateful component and imported the VideoList component and YTSearch module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search'
 const { API_KEY } = require('../api_key')
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: []
+    }
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
